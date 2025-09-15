84a7f7e665f95aa2782173e1d9618680de426a1b
Heitor147 <54001761+Heitor147@users.noreply.github.com>
2025-09-15 09:08:34 -0300

    Update Contador.jsx

--- Técnico/Disciplinas/DW2/3 Bimestre/contador/src/components/Contador.jsx
+++ Técnico/Disciplinas/DW2/3 Bimestre/contador/src/components/Contador.jsx
@@ -1,13 +1,25 @@
 import "./contador.css";
+import React, { useState } from 'react'; 
 
 export default function Contador() {
+  
+  let [contador, setContador] = useState(0)
+
+  function incrementar(){
+    setContador(contador => contador + 1)
+  }
+
+  function decrementar(){
+    setContador(contador => contador - 1)
+  }
+  
   return (
     <div className="container">
       <h1>Contador</h1>
-      <div className="contador-display">0</div>
+      <div className="contador-display">{ contador }</div>
       <div className="botaoContainer">
-        <button className="botao">-</button>
-        <button className="botao">+</button>
+        <button className="botao" onClick={ decrementar}>-</button>
+        <button className="botao" onClick={ incrementar}>+</button>
       </div>
       <button className="botao">Reset</button>
     </div>
