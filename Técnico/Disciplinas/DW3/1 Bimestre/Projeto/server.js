7904f743c2e46b876190b031cd6028d6c745624b
Heitor147 <heitorhenriqur523@gmail.com>
2026-03-18 11:40:11 -0300

    Aula 18/03/2026 11:40

---
+++ Técnico/Disciplinas/DW3/1 Bimestre/Projeto/server.js
@@ -0,0 +1,21 @@
+import Fastify from 'fastify'
+
+const server = Fastify({
+  logger: true
+})
+
+server.get('/', async (request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+server.get('/teste', async (request, reply) => {
+   let x = 100
+   reply.send( x*2 )
+})
+
+try {
+  await server.listen({ port: 3000 })
+} catch (err) {
+  server.log.error(err)
+  process.exit(1)
+}
