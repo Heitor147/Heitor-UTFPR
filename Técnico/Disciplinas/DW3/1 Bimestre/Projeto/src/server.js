b3de0435b2bbd9982dee4fe267295b37d0b66318
Heitor147 <54001761+Heitor147@users.noreply.github.com>
2026-04-06 11:16:39 -0300

    Aula 06/04/2026 11:16

---
+++ Técnico/Disciplinas/DW3/1 Bimestre/Projeto/src/server.js
@@ -0,0 +1,32 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+const server = Fastify()
+
+// Habilita o CORS para permitir requisições do Frontend
+server.register(cors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS']
+})
+
+server.setNotFoundHandler((request, reply) => {
+    reply.code(404).send({
+        status: 'error',
+        message: 'O recurso solicitado não existe nesta API.',
+    })
+})
+
+
+const PORT = 3000
+
+const start = async () => {
+    try {
+        await server.listen({ port: PORT })
+        console.log(`Servidor rodando em http://localhost:${PORT}`)
+    } catch (erro) {
+        console.error(erro)
+        process.exit(1)
+    }
+}
+
+start()
